c5b4ee95aed18fc07e98bfa595ef4c52a9caf8c4
Rian-rgb <rian3903@gmail.com>
2024-12-13 15:05:25 +0700

    task-1 add SecurityFilterChain

--- src/main/java/com/aegisultimateknologi/simple_ecommerce_service/config/SecurityConfig.java
+++ src/main/java/com/aegisultimateknologi/simple_ecommerce_service/config/SecurityConfig.java
@@ -1,20 +1,37 @@
 package com.aegisultimateknologi.simple_ecommerce_service.config;
 
 import com.aegisultimateknologi.simple_ecommerce_service.service.impl.UserDetailsServiceImpl;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain mySFC(HttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        registry -> registry
+                                .anyRequest().permitAll()
+                )
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
